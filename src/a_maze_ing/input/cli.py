8f5388bf05fb9f76d8accbab8de7fa5ad9a0399d
nojishin <154789190+nojishin@users.noreply.github.com>
2026-09-18 16:45:46 +0900

    feat: Pathを戻り値にする

--- src/a_maze_ing/input/cli.py
+++ src/a_maze_ing/input/cli.py
@@ -3,13 +3,14 @@ from pathlib import Path
 
 from a_maze_ing.errors.exceptions import InvalidArgumentsCountError
 
+EXPECTED_ARGC = 2
+
 
 def get_config_path() -> Path:
-    print(sys.argv, len(sys.argv))  # ! test
     argv_len = len(sys.argv)
-    if argv_len != 2:
+    if argv_len != EXPECTED_ARGC:
         raise InvalidArgumentsCountError(argv_len)
-    
+    return Path(sys.argv[1])
 
 
 get_config_path()
